1c382d0c3e669cdb91e747075409bb77d5bdd3da
Andrey Sinetskiy <sinetskiy@gmail.com>
2017-03-21 19:44:13 +0300

    init

---
+++ MVC/router.js
@@ -0,0 +1,11 @@
+var Router = {
+    handle: function(route) {
+        var routeName = route + 'Route';
+
+        if (!Controller.hasOwnProperty(routeName)) {
+            throw new Error('Маршрут не найден!');
+        }
+
+        Controller[routeName]();
+    }
+};
